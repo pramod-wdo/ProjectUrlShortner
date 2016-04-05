0b7defa76107783daf07dedb5c84fccaa0c9c73b
pramod-wdo <pramodhc007@gmail.com>
2016-04-06 00:37:07 +0530

    ->adds users session ->adds user validation ->adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   root 'home#home'
 
+  get '/signup' => 'users#new'
+
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
